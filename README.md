07ab0182dc88cd0aebbf9297c6aa17f24ec8a7d6
Joey <jdechant970@gmail.com>
2018-07-15 15:32:01 -0600

    readme, gifs made

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Diced-Onions
+# Diced Onions
+
+**DESCRIPTION:** App that scrapes recent political articles from the Onion and allows users to leave their comments
+
+![App Tutorial](Diced-Onions.gif)
+
+**USER EXPERIENCE**
+
+* Upon loading of the app, all articles will load from the mongoose Articles collection.
+* If the collection is empty, the user will have the option of making the first scrape by clicking the scrape button.
+* By hovering over an article tab, an Onion logo button will appear that can redirect the User to the actual article.
+* By clicking anywhere on the article tab except the logo button, the comment thread for that article will load.
+* The user has the ability to post a comment by leaving their name along with their comment.
+* At anytime, the user can click "Refresh Results" to scrape for any new articles on the Onion.
+
+
+**PROGRAMMING / FUNCTIONALITY**
+
+* App is built using the following languages/technologies:
+  - *HTML* 
+  - *CSS* 
+  - *JavaScript*
+  - *jQuery*
+  - *Handlebars*
+  - *Node.js*
+  - *MongoDB*
+  - *Mongoose*
+  - *Express*
+  
+* Web scraping is facilitated using Cheerio and Axios middleware.
+
+
+**FILE STRUCTURE**
+
+```
+.
+├── README.md
+├── models
+│   ├── article.js
+│   ├── comment.js
+│   └── index.js
+├── package-lock.json
+├── package.json
+├── public
+│   └── assets
+│       ├── css
+│       │   ├── reset.css
+│       │   └── style.css
+│       ├── images
+│       │   ├── favicon.ai
+│       │   └── favicon.png
+│       └── javascript
+│           └── functions.js
+├── server.js
+└── views
+    ├── articles.handlebars
+    └── layouts
+        └── main.handlebars
+```
--- public/assets/javascript/functions.js
+++ public/assets/javascript/functions.js
@@ -1,3 +1,4 @@
+//scrapes onion, posts new articles to Article collection
 function scrapeOnion(){
     $.get("/api/articles", function(data){
         console.log("data" + data);
@@ -7,35 +8,32 @@ function scrapeOnion(){
         console.log(err);
     });
 }
+//checks to see if there are articles displayed on the page
 function countArticles(){
     var articleLength = $(".articles-section > .article-thumbnail").length;
     if(articleLength > 0){
-        console.log("yes");
     } else {
-        console.log(articleLength);
         var scrapeButton = $("<button class='scrape-button' type='submit'>").text("Click to scrape the Onion!")
         var message = $("<div class='scrape-message'>").text("No articles yet, click the button above to grab some!")
         $(".articles-section").append(scrapeButton, message);
     }
 }
-
+//gets width of the articles sidebar
 function setWrapWidth (){
     var width = $(".articles-section").width() - 100;
-    console.log(width);
-
     $(".text-wrap").css("width", ""+width+"px");
 }
+
+//assigns active class to clicked article, removes class from all others
 function setActiveArticle(){
     const dataId = $(this).attr("data-id");
     $(".active-article").removeClass("active-article");
     $(this).addClass("active-article");
     $(".send-comment").attr("data-id", dataId);
-
-    console.log(dataId);
-
     loadComments(dataId);
 }
 
+//get request that targets Comment collection where articleId = data id for active article
 function loadComments(dataId){
     $(".posted-comment-wrap").empty();
     $("input.comment-name").val("");
@@ -46,12 +44,11 @@ function loadComments(dataId){
         console.log(data);
         for(let i = 0; i < data.length; i++){
         var newDate = moment(data[i].time).format("MM/DD/YYYY");
-        console.log(newDate);
         const comment = $("<div class='comment-wrap'>");
         const details = $("<div class='comment-details'>");
         const name = $("<p class='comment-name'>").text(data[i].name);
         const commentDate = $("<p class='comment-date'>").text(moment(data[i].time).format("MM/DD/YY h:mm a"));
-        const body = $("<p class='comment-body'>").text('"' + data[i].body + '"');
+        const body = $("<p class='comment-body'>").text(data[i].body);
         details.append(name, commentDate);
         comment.append(details, body); 
         $(".posted-comment-wrap").append(comment)
@@ -62,7 +59,7 @@ function loadComments(dataId){
         console.log(err);
     })
 }
-
+//posts new comment to Comment collection
 function submitComment(){
     var time = new Date().getTime()
     var newComment = {
@@ -73,17 +70,15 @@ function submitComment(){
     }
     var query = "api/comments/" + newComment.articleId;
     $.post(query, newComment, function(){
-        console.log(newComment);
     }).then(function(){
         loadComments(newComment.articleId);
-
     }).catch(function(err){
         console.log(err);
     });
 
 }
 
-
+//click events, load events
 $(".refresh-button").on("click" , scrapeOnion);
 $(document).on("click", ".scrape-button" ,  scrapeOnion);
 $(".article-thumbnail").on("click", setActiveArticle);
